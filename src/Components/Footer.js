73c5a86935b5e5b75646eeb68d437ee0b51cf56f
Hamzaraouah <mouradraouah12@gmail.com>
2024-06-01 11:50:22 +0100

    first commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer>
       <h4>Developed by HAMZA RAOUAH</h4>
-      <h4>Copyright &copy; 2024 DS</h4>
+      <h4>Copyright &copy; 2024 HR</h4>
       <div className='footerLinks'>
         <a href="https://github.com/Hamzarhaouah" target='_blank'><FaGithub/></a>
         <a href="https://www.linkedin.com/in/hamza-raouah-623882257/" target='_blank'><FaLinkedin/></a>
